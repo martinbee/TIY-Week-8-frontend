cee847c112cc41a7d551f1b7233fd77ec768e9d5
Martin Bee <martbee88@gmail.com>
2015-06-28 18:55:49 -0500

    finalized

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    response = Http.get("http://localhost:3000/posts.json")
+    response = Http.get("http://localhost:3000/skeletons.json")
     @posts = JSON.parse response
     @user_name = session[:user_name]
   end
@@ -10,9 +10,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    @response = Http.post("http://localhost:3000/posts.json", json: {post:{content: params[:post][:content], user_id: params[:post][:user_id]}})
+    @response = Http.post("http://localhost:3000/skeletons.json", json: {skeleton:{content: params[:post][:content], user_id: params[:post][:user_id]}})
     @posts = JSON.parse @response
-    render root_path
   end
 
 end
