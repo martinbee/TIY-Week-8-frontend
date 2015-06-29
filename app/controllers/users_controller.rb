eacfb2742698ec56de3cc3d70e39241c37d8e433
Martin Bee <martbee88@gmail.com>
2015-06-28 19:49:00 -0500

    meh

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    @response = Http.post("http://localhost:3000/users.json", json: {user:{email: params[:user][:email], password: params[:user][:password], password: params[:user][:password_confirmation]}})
+    @response = Http.post("https://skeletons-tiy-backend.herokuapp.com/users.json", json: {user:{email: params[:user][:email], password: params[:user][:password], password: params[:user][:password_confirmation]}})
   end
 end
