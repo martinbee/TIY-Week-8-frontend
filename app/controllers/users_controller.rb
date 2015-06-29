7817fc58bcc2f8811b9a9710186974b7f8966bb1
Martin Bee <martbee88@gmail.com>
2015-06-29 12:58:22 -0500

    fixed params for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    @response = Http.post("https://skeletons-tiy-backend.herokuapp.com/users.json", json: {user:{email: params[:user][:email], password: params[:user][:password], password: params[:user][:password_confirmation]}})
+    @response = Http.post("https://skeletons-tiy-backend.herokuapp.com/users.json", json: {user:{email: params[:user][:email], password: params[:user][:password], password_confirmation: params[:user][:password_confirmation]}})
   end
 end
